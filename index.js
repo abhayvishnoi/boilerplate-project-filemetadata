41315b272d69cce27fad0d5f5a8b9c3fb71ec323
Abhay VIshnoi <abhayvishnoi6@gmail.com>
2024-10-07 17:23:20 +0000

    fixed

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+const multer  = require('multer')
+const upload = multer()
+var bodyParser=require('body-parser')
 var app = express();
-
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse',upload.single('upfile'), function (req, res) {
+  console.log(req.file);
+  res.json({name:req.file.originalname,type:req.file.mimetype,size:req.file.size})
 });
 
 
